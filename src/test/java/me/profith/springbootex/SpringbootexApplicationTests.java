df25daeef3f1e1df85e691bba36fabdabd7534ef
MoonHKLee <mf311110@gmail.com>
2020-03-21 21:30:54 +0900

    Initial commit

---
+++ src/test/java/me/profith/springbootex/SpringbootexApplicationTests.java
@@ -0,0 +1,13 @@
+package me.profith.springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
